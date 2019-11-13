50cdc2ee0fffa1299ce40def494d58ea6ff64140
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-11-13 00:31:53 -0300

    chore: Added Header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyled from './styles/global';
 import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyled />
-      {/* <Header /> */}
+      <Header />
       <Routes />
     </BrowserRouter>
   );
