113342300f8490506c9942d5d5967f88859cc0d2
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-11-13 00:16:33 -0300

    chore: Added Global Styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyled from './styles/global';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyled />
       {/* <Header /> */}
       <Routes />
     </BrowserRouter>
